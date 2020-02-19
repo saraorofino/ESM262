5af0456806cf1b9e6d206f03d450b07b4b0afad2
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-02-19 10:11:56 -0800

    Add error checking

--- working/function_examples/energy_mixes.R
+++ working/function_examples/energy_mixes.R
@@ -4,10 +4,19 @@
 # N is the amount of energy in TWh that you want to draw (i.e. the global energy demand)  
 # Each time a type of energy drawn it represents 1 TWh of energy from that source
 
-energy_mixes <- function(energy){
-  n <- 23000 # define the amount of energy in TWh needed to meet global energy demand
-  probs = c(0.30, 0.50, 0.50, 0.25, 0.15, 0.45, 0.20) # probability for each type - must be the same length/in the same order as 'energy'
-  out <- sample(energy, size = n, replace = TRUE, prob = probs) # randomly draw a mix of energy to meet global demand
+
+
+energy_mixes <- function(energy, demand, probs){
+  library(tidyverse)
+  if(length(probs) != length(energy)){
+    return("length of probabilities must match energy types")
+  }
+  
+  if(demand < 0){
+    return("energy demand must be positive")
+  }
+ 
+  out <- sample(energy, size = demand, replace = TRUE, prob = probs) # randomly draw a mix of energy to meet global demand
   return(as.data.frame(out) %>% 
            rename(energy_type = "out") %>%
            group_by(energy_type) %>% # determine the amount of energy in TWh from each source
@@ -16,10 +25,16 @@ energy_mixes <- function(energy){
            mutate(percent_of_demand = round(((TWh/23000)*100), 2))) # calculate what percent of the demand comes from each source
 }
 
+
+
 # Test the function:
-# Create your vector of energy types:
+# Create your vector of energy types, size and probabilty:
 # cc = carbon capture, s = solar, w = wind, sw = solar/wind co-generation, g = geothermal, b = biofuels, n = nuclear
 energy_types <- c("cc", "s", "w", "sw", "g", "b", "n")
+global_demand <- 23000 #definee the amount of energy in TWh needed to meet global energy demand
+energy_prob <- c(0.30, 0.50, 0.50, 0.25, 0.15, 0.45, 0.20) # probability for each type - must be the same length/in the same order as 'energy'
 
-mix1 <- energy_mixes(energy = energy_types)
+mix1 <- energy_mixes(energy = energy_types, demand = global_demand, probs = energy_prob)
 mix2 <- energy_mixes(energy = energy_types)
+
+
--- working/function_examples/zscore_index.R
+++ working/function_examples/zscore_index.R
@@ -4,6 +4,11 @@
 # Function to compute the z-score for a single vector
 # Can be used alone for just one variable or run to feed into the z-score index function below
 z_score <- function(vec) {
+  
+  if(length(vec) == 1){
+    return("input must be a vector") #Return error message if the input is a single value
+  }
+  
   # Can only use on numeric variables 
   if(is.numeric(vec)==T){
     vec <- vec
@@ -20,6 +25,8 @@ z_score <- function(vec) {
 # Test the function:
 test_data <- c(NA, rnorm(10), NA) # randomly draw a vector of 10 numbers from a normal distribution, include NA's to test function
 z_test <- z_score(vec = test_data)
+z_score(vec = test_data)
+
 
 # Use this function to compute a z-score index that combines the z-scores of multiple columns 
 z_score_index <- function(dta, vars){
