8c51016be4ecbfd1b47990abb6e420175b97524d
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-02-26 09:57:39 -0800

    Set up script and rmd for assignment 2

---
+++ assignments/assignment2/fish_revenue.R
@@ -0,0 +1,4 @@
+# Function to find the most frequently caught fish at each location, the total revenue for each fishery, and total revenue by location
+# Inputs:
+## price - a table of prices for different fish
+## fish_data - a table of the number of fish caught in each species at each location 
