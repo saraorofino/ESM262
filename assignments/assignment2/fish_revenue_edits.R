d83474679726d3fff3d7609ee74db7b79ec52179
Chase Brewster <chasewynnsbrewster@gmail.com>
2020-03-01 20:23:34 -0800

    added a couple things

---
+++ assignments/assignment2/fish_revenue_edits.R
@@ -0,0 +1,51 @@
+# Function to find the most frequently caught fish at each location, the total revenue for each fishery, and total revenue by location
+# Inputs:
+## price - a table of prices for different fish
+## fish_data - a table of the number of fish caught in each species at each location 
+
+
+#Outputs:
+#most frequently caught fish in each location
+#total revenue for each location
+#total fisheries revenue sum
+
+fish_revenue <- function(fish_data, price){
+  
+  #create the results table - length should only be the number of locations
+  places <- c("Alaska", "California", "Mexico", "North Carolina", "Maine")
+  
+  results <- data.frame(location = rep(NA, length(places)),
+                        common_fish = rep(NA, length(places)),
+                        total_rev = rep(NA, length(places)))
+  
+  for(i in 1:length(places)){
+    
+    results$location[i] <- places[i] #set the location
+    
+    #Subset by location to find which fish had the highest catch for each location
+    results$common_fish[i] <- fish_data$fish[which.max(fish_data$places[i])]
+    
+    location_value <- results$location[i]
+    sub_location <- subset(fish_data, location == location_value)
+    sub_max <- sub_location$fish[which.max(sub_location$catch)]
+    results$common_fish[i] <- sub_max
+    
+    #Calculate the revenue for each location - add a price column to the subsetted data, find price for each fish & calc revenue
+    for(f in 1:7){
+    fish <- sub_location$fish[f] 
+    fish.price <- price$price[price$fish == fish]
+    sub_location$price[f] <- fish.price
+    fish.catch <- sub_location$catch[f]
+    sub_location$revenue[f] <- fish.price * fish.catch
+    }
+    
+    total_rev <- sum(sub_location$revenue)
+    results$total_rev[i] <- total_rev
+  }
+  
+  return(results)
+}
+
+
+#Test:
+test <- fish_revenue(fish_data = catch, price = fish_prices)
