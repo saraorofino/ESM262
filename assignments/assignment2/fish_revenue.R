3e9f62f6a71cebab1992d095953063638f40614d
Sara Orofino <saraorofino@bren.ucsb.edu>
2020-03-01 20:32:49 -0800

    Get function to calculate the revenue based on location and find most common fish by location

--- assignments/assignment2/fish_revenue.R
+++ assignments/assignment2/fish_revenue.R
@@ -7,9 +7,9 @@
 #Outputs:
 #most frequently caught fish in each location
 #total revenue for each location
-#total fisheries revenue sum
+#total fisheries revenue sum -- this doesn't make sense to me based on every other output being location dependent
 
-fish_revenue <- function(fish_data, price){
+fish_revenue <- function(fish_data, fish_prices){
   
   #create the results table - length should only be the number of unique values for the location
   unique_loc <- unique(fish_data$location)
@@ -23,20 +23,20 @@ fish_revenue <- function(fish_data, price){
     results$location[i] <- unique_loc[i] #set the location
     
     #Subset by location to find which fish had the highest catch for each location
-    location_value <- results$location[i]
-    sub_location <- subset(fish_data, location == location_value)
+    location_name <- results$location[i]
+    sub_location <- subset(fish_data, location == location_name)
     sub_max <- sub_location$fish[which.max(sub_location$catch)]
     results$common_fish[i] <- sub_max
     
     #Calculate the revenue for each location - add a price column to the subsetted data, find price for each fish & calc revenue
     for(f in 1:7){
-    fish <- sub_location$fish[f] 
-    fish.price <- price$price[price$fish == fish]
-    sub_location$price[f] <- fish.price
+    fish.name <- sub_location$fish[f]
+    fish.price <- fish_prices$price[fish_prices$fish == fish.name]
+    sub_location$fish_price[f] <- fish.price
     fish.catch <- sub_location$catch[f]
     sub_location$revenue[f] <- fish.price * fish.catch
     }
-    
+
     total_rev <- sum(sub_location$revenue)
     results$total_rev[i] <- total_rev
   }
@@ -46,4 +46,4 @@ fish_revenue <- function(fish_data, price){
 
 
 #Test:
-test <- fish_revenue(fish_data = catch, price = prices)
+test <- fish_revenue(fish_data = catch, fish_prices = fish.prices)
