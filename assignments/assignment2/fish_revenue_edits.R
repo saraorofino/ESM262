3f0cd9061256c51aa70fdca29645a04c7ee23de6
Chase Brewster <chasewynnsbrewster@gmail.com>
2020-03-06 14:11:40 -0800

    parts 1 and 2 complete

--- assignments/assignment2/fish_revenue_edits.R
+++ assignments/assignment2/fish_revenue_edits.R
@@ -9,43 +9,67 @@
 #total revenue for each location
 #total fisheries revenue sum
 
-fish_revenue <- function(fish_data, price){
+fish_revenue <- function(catch_data, price_data){
+  
+  #create lists for lengths
+  #one of locations
+  #one of fish types
+  places <- c("Alaska", "California", "Mexico", "North Carolina", "Maine")
+  fish <- rownames(catch_data)
+  
+  #transpose catch data, to be able to use which.max
+  catch_data_t <- t(catch_data)
   
   #create the results table - length should only be the number of locations
-  places <- c("Alaska", "California", "Mexico", "North Carolina", "Maine")
-  
   results <- data.frame(location = rep(NA, length(places)),
                         common_fish = rep(NA, length(places)),
                         total_rev = rep(NA, length(places)))
   
+  #use which.max on transposed data, for every row this function finds the column with the highest value, and then takes the column name (in this case, the fish name)
+  common_fish <- colnames(catch_data_t)[apply(catch_data_t, 1, which.max)]
+  
+  #insert the most commmon fish by location into the results data frame
+  results$common_fish <- common_fish
+  
+  #creat column names to make sure the function calls the correct information from the input data set
+  col_names <- c("fish", "price")
+  
+  #set the column names of the price_data input so that when we call from the data, it actually grabs the price info
+  colnames(price_data) <- col_names
+  
+  #create a new dataset, identical to the input catch data, that we can then manipulate to get the info we need
+  catch_data_2 <- catch_data
+  
+  #add a column to the new catch data frame, that pulls the price info from the fish price data
+  catch_data_2$price <- price_data$price
+  
+  #create an empty column in the new catch data frame, to fill up with catch revenues for the fish during the for loop
+  catch_data_2$fish_rev <- rep(NA, length(fish))
+  
+  #now, run a loop for each location, with a nested loop for each fish, to calculate total revenue by location
   for(i in 1:length(places)){
     
-    results$location[i] <- places[i] #set the location
+    #fill in the location column of the results dataframe
+    results$location[i] <- places[i]
     
-    #Subset by location to find which fish had the highest catch for each location
-    results$common_fish[i] <- fish_data$fish[which.max(fish_data$places[i])]
-    
-    location_value <- results$location[i]
-    sub_location <- subset(fish_data, location == location_value)
-    sub_max <- sub_location$fish[which.max(sub_location$catch)]
-    results$common_fish[i] <- sub_max
-    
-    #Calculate the revenue for each location - add a price column to the subsetted data, find price for each fish & calc revenue
-    for(f in 1:7){
-    fish <- sub_location$fish[f] 
-    fish.price <- price$price[price$fish == fish]
-    sub_location$price[f] <- fish.price
-    fish.catch <- sub_location$catch[f]
-    sub_location$revenue[f] <- fish.price * fish.catch
+    #Calculate the revenue for each fish catch at each location, entered into the blank column of the new catch data frame
+    for(f in 1:length(fish)){
+      #multiply the price of the fish * the amount of that fish caught at that location, and enter it temporarily into the data frame
+      catch_data_2$fish_rev[f] <- catch_data_2$price[f]*catch_data_2[f, i]
     }
-    
-    total_rev <- sum(sub_location$revenue)
-    results$total_rev[i] <- total_rev
+    #sum the revenue of each fish at that location
+    rev <- sum(catch_data_2$fish_rev)
+    #record the total fishery revenue at that location into the results data frame, for every location
+    results$total_rev[i] <- rev
   }
-  
+  #create column names for the results data frame
+  result_colnames <- c("Location", "Most Frequent Fish", "Total Fisheries Revenue")
+  #apply those column names
+  colnames(results) <- result_colnames
+  #return the results
   return(results)
 }
 
 
 #Test:
-test <- fish_revenue(fish_data = catch, price = fish_prices)
+#test <- fish_revenue(catch_data = catch_data, price_data = fish_prices)
